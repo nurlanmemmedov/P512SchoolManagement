914d0bfe9b82e67153c33b23e85e25e436542898
Nurlan Mammadov <nmammadov@kibrit.tech>
2022-04-16 18:33:54 +0400

    Create migration for className property

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,29 +1,29 @@
-﻿using Authentication.Models.Dtos;
-using Authentication.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Authentication.Models.Dtos;
+//using Authentication.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
-    {
-        private readonly IAuthService _service;
+//namespace Api.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class AuthController : ControllerBase
+//    {
+//        private readonly IAuthService _service;
 
-        public AuthController(IAuthService service)
-        {
-            _service = service;
-        }
+//        public AuthController(IAuthService service)
+//        {
+//            _service = service;
+//        }
 
-        [HttpPost("login")]
-        public async Task<LoginResponseDto> Login([FromBody] LoginModelDto model)
-        {
-            return await _service.Login(model);
-        }
-    }
-}
+//        [HttpPost("login")]
+//        public async Task<LoginResponseDto> Login([FromBody] LoginModelDto model)
+//        {
+//            return await _service.Login(model);
+//        }
+//    }
+//}
---
+++ Data/Migrations/20220416143256_AddClassnameProp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddClassnameProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClassName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClassName",
+                table: "Students");
+        }
+    }
+}
